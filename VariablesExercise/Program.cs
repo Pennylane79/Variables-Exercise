40209c43c40e3fed29774e3e4bf5ece2b7ac72ed
Pennylane79 <wrichard3417@hotmail.com>
2025-04-15 16:17:37 -0500

    added variable exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dolly = "dolly";
+            int wholeNumber = 1;
+            char letter = 'a';
+            bool house
+                = true;
+            double cotton = 3.140432;
+            decimal money = 147.02m;
+            Console.WriteLine($"Hello, {dolly}!she is {wholeNumber} and {letter} and {cotton} and {money} and {house}");
         }
     }
 }
